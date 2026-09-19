2668483be2bd53948a3b1baca232521c7d739c73
Tapout341 <marcira.irabagon@ust.edu.ph>
2026-09-19 22:26:20 +0800

    feat: added top 10 rankings

--- summary.py
+++ summary.py
@@ -13,5 +13,8 @@ class datasummarizer:
             row_count='size', valid_measure_count='count', sum='sum', mean='mean'
         ).reset_index()
         grouped.to_csv(output_dir / "grouped.csv", index=False)
+        top10 = grouped.sort_values(by="sum", ascending=False).head(10)
+        top10.to_csv(output_dir / "top10.csv", index=False)
+        return grouped, top10
 
 
