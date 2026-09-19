ac88f8fdca9be560ac716cd781353f2577916cc8
Tapout341 <marcira.irabagon@ust.edu.ph>
2026-09-19 22:17:22 +0800

    feat: created new class datasummary

--- summary.py
+++ summary.py
@@ -1,2 +1,10 @@
 from pathlib import Path
 import pandas as pd
+
+class datasummarizer:
+  def __init__(self, df: pd.Dataframe, cat1: str, cat2:str, measure: str):
+    self.df = df
+    self.cat1 = cat1
+    self.cat2 = cat2
+    self.measure = measure
+
